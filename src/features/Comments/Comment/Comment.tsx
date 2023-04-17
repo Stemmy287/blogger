27b8190322d85f5f597cfa781729e0d531d2b302
Stemmy287 <steem287@gmail.com>
2023-04-17 11:43:16 +0300

    feat: edit comment flow

--- src/features/Comments/Comment/Comment.tsx
+++ src/features/Comments/Comment/Comment.tsx
@@ -1,9 +1,13 @@
-import React, {FC} from 'react';
+import React, {ChangeEvent, FC, useState} from 'react';
 import s from 'features/Comments/Comment/comment.module.scss'
 import noPhoto from 'common/image/no-image.svg'
-import {BurgerMenu} from "common/components/BurgerMenu/BurgerMenu";
 import {dateConvertor} from "common/utils/dateConvertor";
 import {CommentType} from "features/Comments/commentsApi";
+import {FormInput} from "common/components/FormInput/FormInput";
+import {Button} from "common/components/Button/Button";
+import { BurgerMenu } from 'common/components/BurgerMenu/BurgerMenu';
+import {useAppDispatch} from "hooks/useAppDispatch";
+import {updateCommentTC} from "features/Comments/commentsSlice";
 
 type PropsType = {
   comment: CommentType
@@ -13,11 +17,34 @@ type PropsType = {
 
 export const Comment: FC<PropsType> = ({comment, setPopUpActive, setCommentId}) => {
 
+  const dispatch = useAppDispatch()
+
   const {userLogin, createdAt, content, id} = comment
+
+  const [editValue, setEditValue] = useState(content)
+  const [isEdit, setIsEdit] = useState(false)
+
+  const onEditHandler = () => {
+    dispatch(updateCommentTC({commentId: id, content: editValue}))
+    setIsEdit(false)
+  }
 
   const onDeleteHandler = () => {
     setPopUpActive(true)
     setCommentId(id)
+  }
+
+  const onEditActiveHandler = () => {
+    setIsEdit(true)
+  }
+
+  const onCancelHandler = () => {
+    setIsEdit(false)
+    setEditValue(content)
+  }
+
+  const onChangeValue = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
+    setEditValue(e.currentTarget.value)
   }
 
   return (
@@ -28,12 +55,17 @@ export const Comment: FC<PropsType> = ({comment, setPopUpActive, setCommentId}) 
           <h2 className={s.name}>{userLogin}</h2>
           <span className={s.date}>{dateConvertor(createdAt)}</span>
         </div>
-        <p>{content}</p>
+        {isEdit ? <FormInput value={editValue} onChange={onChangeValue} component="textarea" /> : <p>{content}</p>}
+        {isEdit && <div className={s.buttons}>
+          <Button isNoBackGround title="Cancel" callback={onCancelHandler}/>
+          <Button disabled={content === editValue} title="Edit Comment" callback={onEditHandler}/>
+        </div>}
       </div>
       <div className={s.burger_menu}>
-        <BurgerMenu
-          onEditClick={() => {}}
-          onDeleteClick={onDeleteHandler}/>
+        {!isEdit && <BurgerMenu
+          onEditClick={onEditActiveHandler}
+          onDeleteClick={onDeleteHandler}
+        />}
       </div>
     </div>
   );
