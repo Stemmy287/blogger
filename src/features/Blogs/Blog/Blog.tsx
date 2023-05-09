04bbc31214207dd48a5a89e5e3157c8dd2b3c704
Stemmy287 <steem287@gmail.com>
2023-05-09 13:28:25 +0300

    fix: change variable color

--- src/features/Blogs/Blog/Blog.tsx
+++ src/features/Blogs/Blog/Blog.tsx
@@ -4,31 +4,31 @@ import {NavLink} from "react-router-dom";
 import defaultBlogImage from 'common/image/blog2.png'
 
 type BlogsPropsType = {
-    blogId: string
-    title: string
-    webSiteUrl: string
-    description: string
+  blogId: string
+  title: string
+  webSiteUrl: string
+  description: string
 }
 
 export const Blog: FC<BlogsPropsType> = ({
-                                             blogId,
-                                             title,
-                                             webSiteUrl,
-                                             description,
+                                           blogId,
+                                           title,
+                                           webSiteUrl,
+                                           description,
                                          }) => {
-    return (
-        <div className={s.blogContainer}>
-            <div className={s.photo}>
-                <img src={defaultBlogImage} alt="blog image"/>
-            </div>
-            <div className={s.content}>
-                <NavLink to={`/BlogPage/${blogId}`} className={s.navBlog}>
-                    <h3 className={s.title}>{title}</h3>
-                </NavLink>
-                <span className={s.website}><b>Website:</b><a href="">{webSiteUrl}</a></span>
-                <span className={s.text}>{description}</span>
-            </div>
-        </div>
-    );
+  return (
+    <div className={s.blogContainer}>
+      <div className={s.photo}>
+        <img src={defaultBlogImage} alt="blog image"/>
+      </div>
+      <div className={s.content}>
+        <NavLink to={`/BlogPage/${blogId}`} className={s.navBlog}>
+          <h3 className={s.title}>{title}</h3>
+        </NavLink>
+        <span className={s.website}><b>Website:</b><a href="">{webSiteUrl}</a></span>
+        <span className={s.text}>{description}</span>
+      </div>
+    </div>
+  );
 };
 
