ab18f95698cf969e97dd38010b6d36370801993b
Stemmy287 <steem287@gmail.com>
2023-05-15 14:42:21 +0300

    feat: hook useSearch

---
+++ src/hooks/useSearch.ts
@@ -0,0 +1,15 @@
+import {useEffect, useState} from "react";
+import {useDebounce} from "hooks/useDebounce";
+
+export const useSearch = (searchHandler: (debouncedSearchValue: string) => void) => {
+
+  const [searchValue, setSearchValue] = useState('')
+
+  const debouncedSearchNameTerm = useDebounce(searchValue, 750)
+
+  useEffect(() => {
+    searchHandler(debouncedSearchNameTerm)
+  }, [debouncedSearchNameTerm])
+
+  return {searchValue, setSearchValue}
+}
