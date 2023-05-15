023bffca20883f004281c154a308ec67391866b5
Stemmy287 <steem287@gmail.com>
2023-05-15 15:15:14 +0300

    fix: blog title

--- src/features/Blogs/Blog/Blog.tsx
+++ src/features/Blogs/Blog/Blog.tsx
@@ -2,6 +2,7 @@ import React, {FC} from 'react';
 import s from './blog.module.scss'
 import {NavLink} from "react-router-dom";
 import defaultBlogImage from 'common/image/defaultBlogImg.png'
+import {currentURL} from "common/utils/currentURL";
 
 type BlogsPropsType = {
   blogId: string
@@ -16,14 +17,17 @@ export const Blog: FC<BlogsPropsType> = ({
                                            webSiteUrl,
                                            description,
                                          }) => {
+
   return (
     <div className={s.blogContainer}>
       <img src={defaultBlogImage} alt="blog image"/>
       <div className={s.content}>
-        <NavLink to={`/BlogPage/${blogId}`} className={s.navBlog}>
-          <h3 className={s.title}>{title}</h3>
-        </NavLink>
-        <span className={s.website}><b>Website:</b><a href={webSiteUrl} target='_blank'>{webSiteUrl}</a></span>
+        <div className={s.navContainer}>
+          <NavLink to={`/BlogPage/${blogId}`} className={s.navBlog}>
+            <h3 className={s.title}>{title}</h3>
+          </NavLink>
+        </div>
+        <span className={s.website}><b>Website:</b><a href={currentURL(webSiteUrl)} target='_blank'>{currentURL(webSiteUrl)}</a></span>
         <span className={s.text}>{description}</span>
       </div>
     </div>
