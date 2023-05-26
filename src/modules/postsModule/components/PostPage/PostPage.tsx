095b7f4bc8365b38c9800a5feef1419060fdbe8e
Stemmy287 <steem287@gmail.com>
2023-05-26 12:26:05 +0300

    feat: post page refactor

--- src/modules/postsModule/components/PostPage/PostPage.tsx
+++ src/modules/postsModule/components/PostPage/PostPage.tsx
@@ -1,38 +1,54 @@
-import React, {useEffect} from 'react';
-import {Title} from "common/components/Title/Title";
-import {BackLink} from "common/components/BackLink/BackLink";
-import {PostDeployed} from "modules/postsModule/components/PostDeployed/PostDeployed";
-import {useParams} from "react-router-dom";
-import {useAppDispatch} from "hooks/useAppDispatch";
-import {fetchPostTC} from "modules/postsModule/postsSlice";
-import {useAppSelector} from "hooks/useAppSelector";
-import {postSelector} from "modules/postsModule/postsSelectors";
-import {Comments} from "modules/commentsModule/components/Comments/Comments";
-import s from './postPage.module.scss'
+import React, { useEffect } from 'react';
+import { Title } from 'common/components/Title/Title';
+import { BackLink } from 'common/components/BackLink/BackLink';
+import { useParams } from 'react-router-dom';
+import { useAppDispatch } from 'hooks/useAppDispatch';
+import { fetchPostTC } from 'modules/postsModule/postsSlice';
+import { useAppSelector } from 'hooks/useAppSelector';
+import { postSelector } from 'modules/postsModule/postsSelectors';
+import { Comments } from 'modules/commentsModule/components/Comments/Comments';
+import s from './postPage.module.scss';
+import defaultBlogImage from 'common/image/defaultBlogImg.png';
+import defaultPostImage from 'common/image/post-banner.png';
+import { dateConvertor } from '../../../../common/utils/dateConvertor';
 
 export const PostPage = () => {
+	const { postId } = useParams();
+	const post = useAppSelector(postSelector);
+	const dispatch = useAppDispatch();
 
-  const {postId} = useParams()
-  const post = useAppSelector(postSelector)
-  const dispatch = useAppDispatch()
+	useEffect(() => {
+		if (postId) {
+			dispatch(fetchPostTC({ postId }));
+		}
+	}, [postId, dispatch]);
 
-  useEffect(() => {
-    if (postId) {
-      dispatch(fetchPostTC({postId}))
-    }
-  }, [])
+	const date = dateConvertor(post.createdAt, true);
 
-  return (
-    <div className={s.postPageContainer}>
-      <Title title={'Posts'} isDesc={true} desc={post.blogName}/>
-      <BackLink link={'/postsModule'} to={'posts'}/>
-      <PostDeployed
-        blogName={post.blogName}
-        postName={post.title}
-        date={post.createdAt}
-        content={post.content}
-      />
-      <Comments postId={postId || ''}/>
-    </div>
-  );
+	return (
+		<div>
+			<div className={s.mainTitleWrapper}>
+				<Title title={'Posts'} isDesc={true} desc={post.blogName} />
+			</div>
+			<BackLink link={'/postsModule'} to={'posts'} />
+			<div className={s.postDeployedContainer}>
+				<div className={s.blogInfo}>
+					<img className={s.blogAvatar} src={defaultBlogImage} alt="blog avatar" />
+					<h3 className={s.blogTitle}>{post.blogName}</h3>
+				</div>
+				<div className={s.postInfo}>
+					<div className={s.titleWrapper}>
+						<h2 className={s.postTitle}>{post.title}</h2>
+						<span className={s.typeOfPost}>(for public posts)</span>
+					</div>
+					<span className={s.date}>{date}</span>
+				</div>
+				<div className={s.content}>
+					<img className={s.postBanner} src={defaultPostImage} alt={'banner'} />
+					<p className={s.content}>{post.content}</p>
+				</div>
+			</div>
+			<Comments postId={postId || ''} />
+		</div>
+	);
 };
