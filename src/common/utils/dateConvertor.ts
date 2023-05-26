a205eadf0e74abff8a4e744f3e41dec9eb5f8ff9
Stemmy287 <steem287@gmail.com>
2023-05-26 12:25:48 +0300

    feat: date converter with time

--- src/common/utils/dateConvertor.ts
+++ src/common/utils/dateConvertor.ts
@@ -1,3 +1,11 @@
-export const dateConvertor = (date: string) => {
-  return (new Date(Date.parse(date)).toLocaleDateString())
-}
+export const dateConvertor = (date: string, withTime?: boolean) => {
+	const parsedDate = new Date(Date.parse(date)).toLocaleDateString();
+
+	if (withTime) {
+		const parsedTime = new Date(Date.parse(date)).toLocaleTimeString();
+
+		return `${parsedDate.replace(/\./g, '/')} at ${parsedTime}`;
+	}
+
+	return parsedDate;
+};
