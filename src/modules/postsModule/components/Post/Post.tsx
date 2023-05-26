7a539d850df7451550d59223320051278ae51419
Stemmy287 <steem287@gmail.com>
2023-05-26 11:26:07 +0300

    feat: postPage refactor

--- src/modules/postsModule/components/Post/Post.tsx
+++ src/modules/postsModule/components/Post/Post.tsx
@@ -1,45 +1,32 @@
-import React, {FC} from 'react';
-import s from './post.module.scss'
-import {NavLink} from "react-router-dom";
-import defaultPostImage from 'common/image/defaultPostImg.png'
-import defaultBlogImage from 'common/image/defaultBlogImg.png'
-import {dateConvertor} from "common/utils/dateConvertor";
+import React, { FC } from 'react';
+import s from './post.module.scss';
+import { NavLink } from 'react-router-dom';
+import defaultPostImage from 'common/image/defaultPostImg.png';
+import defaultBlogImage from 'common/image/defaultBlogImg.png';
+import { dateConvertor } from 'common/utils/dateConvertor';
 
 type PostPropsType = {
-  postId: string
-  title: string
-  blogName: string
-  date: string
-}
+	postId: string;
+	title: string;
+	blogName: string;
+	date: string;
+};
 
-export const Post: FC<PostPropsType> = ({
-                                          postId,
-                                          title,
-                                          blogName,
-                                          date
-                                        }) => {
+export const Post: FC<PostPropsType> = ({ postId, title, blogName, date }) => {
+	const dateParsed = dateConvertor(date);
 
-
-  const dateParsed = dateConvertor(date)
-
-  return (
-    <div className={s.postContainer}>
-      <div className={s.banner}>
-        <img src={defaultPostImage} alt={'banner'}/>
-      </div>
-      <div className={s.content}>
-        <div className={s.img}>
-          <img src={defaultBlogImage} alt=""/>
-        </div>
-        <div className={s.text}>
-          <NavLink to={`/PostPage/${postId}`} className={s.navPost}>
-            <h3 className={s.title}>{title}</h3>
-          </NavLink>
-          <span className={s.description}>{blogName}</span>
-          <span className={s.date}>{dateParsed}</span>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={s.postContainer}>
+			<img className={s.postBanner} src={defaultPostImage} alt="banner" />
+			<div className={s.content}>
+				<img className={s.blogAvatar} src={defaultBlogImage} alt="blog avatar" />
+				<div className={s.text}>
+					<NavLink to={`/PostPage/${postId}`} className={s.navPost}>{title}</NavLink>
+					<span className={s.description}>{blogName}</span>
+					<span className={s.date}>{dateParsed}</span>
+				</div>
+			</div>
+		</div>
+	);
 };
 
--- src/modules/postsModule/components/Posts/Posts.tsx
+++ src/modules/postsModule/components/Posts/Posts.tsx
@@ -1,73 +1,86 @@
-import React, {useEffect} from 'react';
-import s from './posts.module.scss'
-import {Title} from "common/components/Title/Title";
-import {Post} from "../Post/Post";
-import {useAppSelector} from "hooks/useAppSelector";
-import {fetchPostsTC, setIsPaginationPostsAC, setPageNumberPostsAC, setSortByPostsAC} from "modules/postsModule/postsSlice";
-import {useAppDispatch} from "hooks/useAppDispatch";
+import React, { useEffect, useState } from 'react';
+import s from './posts.module.scss';
+import { Title } from 'common/components/Title/Title';
+import { Post } from '../Post/Post';
+import { useAppSelector } from 'hooks/useAppSelector';
 import {
-  postsPageNumberSelector,
-  postsSelector,
-  postsSortBySelector,
-  postsSortDirectionSelector,
-  postsTotalCountSelector
-} from "modules/postsModule/postsSelectors";
-import {Pagination} from "common/components/Pagination/Pagination";
-import {Navigate} from "react-router-dom";
-import {PATH} from "common/constans/path";
-import {isLoggedInSelector} from "modules/authModule/authSelectors";
+	fetchPostsTC,
+	setIsPaginationPostsAC,
+	setPageNumberPostsAC,
+	setSortByPostsAC,
+} from 'modules/postsModule/postsSlice';
+import { useAppDispatch } from 'hooks/useAppDispatch';
+import {
+	postsPageNumberSelector,
+	postsSelector,
+	postsSortBySelector,
+	postsSortDirectionSelector,
+	postsTotalCountSelector,
+} from 'modules/postsModule/postsSelectors';
+import { Pagination } from 'common/components/Pagination/Pagination';
+import { Navigate } from 'react-router-dom';
+import { PATH } from 'common/constans/path';
+import { isLoggedInSelector } from 'modules/authModule/authSelectors';
+import { Select } from '../../../../common/components/Select/Select';
+import { OptionsSelectorType } from '../../../blogsModule/types';
 
 export const Posts = () => {
+	const posts = useAppSelector(postsSelector);
 
-  const posts = useAppSelector(postsSelector)
+	const pageNumber = useAppSelector(postsPageNumberSelector);
+	const sortBy = useAppSelector(postsSortBySelector);
+	const sortDirection = useAppSelector(postsSortDirectionSelector);
 
-  const pageNumber = useAppSelector(postsPageNumberSelector)
-  const sortBy = useAppSelector(postsSortBySelector)
-  const sortDirection = useAppSelector(postsSortDirectionSelector)
+	const postsTotalCount = useAppSelector(postsTotalCountSelector);
 
-  const postsTotalCount = useAppSelector(postsTotalCountSelector)
+	const isLoggedIn = useAppSelector(isLoggedInSelector);
 
-  const isLoggedIn = useAppSelector(isLoggedInSelector)
+	const dispatch = useAppDispatch();
 
-  const dispatch = useAppDispatch()
+	useEffect(() => {
+		dispatch(fetchPostsTC());
+	}, [pageNumber, sortBy, sortDirection, dispatch]);
 
-  useEffect(() => {
-    dispatch(fetchPostsTC())
-  }, [pageNumber, sortBy, sortDirection])
+	const [options] = useState([
+		{ title: 'New blogs first', value: 'desc createdAt' },
+		{ title: 'Old blogs first', value: 'asc createdAt' },
+	]);
 
-  const onChangeSelect = (sortBy: string) => {
-    const value = sortBy.split(' ')
-    dispatch(setPageNumberPostsAC({pageNumber: 1}))
-    dispatch(setSortByPostsAC({sortBy: value[1], sortDirection: value[0]}))
-  }
+	const onChangeSelect = (data: OptionsSelectorType) => {
+		if (data.value) {
+			const value = data.value.split(' ');
+			dispatch(setPageNumberPostsAC({ pageNumber: 1 }));
+			dispatch(setSortByPostsAC({ sortBy: value[1], sortDirection: value[0] }));
+		}
+	};
 
-  const onPagination = () => {
-    dispatch(setIsPaginationPostsAC())
-    dispatch(setPageNumberPostsAC({pageNumber: pageNumber + 1}))
-  }
+	const onPagination = () => {
+		dispatch(setIsPaginationPostsAC());
+		dispatch(setPageNumberPostsAC({ pageNumber: pageNumber + 1 }));
+	};
 
-  if(!isLoggedIn) {
-    return <Navigate to={PATH.LOGIN}/>
-  }
+	if (!isLoggedIn) {
+		return <Navigate to={PATH.LOGIN} />;
+	}
 
-  return (
-    <div>
-      <Title title={'Posts'} isDesc={false}/>
-      <div className={s.select}>
-
-      </div>
-      <div className={s.posts}>
-        {posts.map(ps =>
-          <Post
-            key={ps.id}
-            postId={ps.id}
-            title={ps.title}
-            blogName={ps.blogName}
-            date={ps.createdAt}
-          />)}
-      </div>
-      {postsTotalCount > posts.length && <div className={s.pagination}><Pagination callback={onPagination}/></div>}
-    </div>
-  );
+	return (
+		<div>
+			<Title title={'Posts'} isDesc={false} />
+			<div className={s.selectWrapper}>
+				<div className={s.select}>
+					<Select title={options[0].title} options={options} onChange={onChangeSelect} />
+				</div>
+			</div>
+			<div className={s.posts}>
+				{posts.map(ps => (
+					<Post key={ps.id} postId={ps.id} title={ps.title} blogName={ps.blogName} date={ps.createdAt} />
+				))}
+			</div>
+			{postsTotalCount > posts.length && (
+				<div className={s.pagination}>
+					<Pagination callback={onPagination} />
+				</div>
+			)}
+		</div>
+	);
 };
-
