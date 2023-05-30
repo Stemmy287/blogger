bece18aa401087eabe394747ec4238562492c1a1
Stemmy287 <steem287@gmail.com>
2023-05-30 13:36:36 +0300

    feat: refactor

--- src/modules/commentsModule/components/Comments/Comments.tsx
+++ src/modules/commentsModule/components/Comments/Comments.tsx
@@ -1,101 +1,111 @@
-import React, {ChangeEvent, FC, useEffect, useState} from 'react';
-import s from 'modules/commentsModule/components/Comments/comments.module.scss'
-import {FormInput} from "common/components/FormInput/FormInput";
-import {Comment} from "modules/commentsModule/components/Comment/Comment";
-import {Pagination} from "common/components/Pagination/Pagination";
-import {useAppDispatch} from "hooks/useAppDispatch";
-import {useAppSelector} from "hooks/useAppSelector";
-import {Button} from "common/components/Button/Button";
+import React, { ChangeEvent, FC, useEffect, useState } from 'react';
+import s from 'modules/commentsModule/components/Comments/comments.module.scss';
+import { FormInput } from 'common/components/FormInput/FormInput';
+import { Comment } from 'modules/commentsModule/components/Comment/Comment';
+import { Pagination } from 'common/components/Pagination/Pagination';
+import { useAppDispatch } from 'hooks/useAppDispatch';
+import { useAppSelector } from 'hooks/useAppSelector';
+import { Button } from 'common/components/Button/Button';
 import {
-  commentsPageNumberSelector,
-  commentsSelector,
-  commentsTotalCountSelector
-} from "modules/commentsModule/commentsSelectors";
+	commentsPageNumberSelector,
+	commentsSelector,
+	commentsTotalCountSelector,
+} from 'modules/commentsModule/commentsSelectors';
 import {
-  createCommentTC, deleteCommentTC,
-  fetchCommentsTC,
-  setIsPaginationCommentsAC,
-  setPageNumberCommentsAC
-} from "modules/commentsModule/commentsSlice";
-import {PopUp} from "common/components/PopUp/PopUp";
-import {Notification} from "common/components/Notification/Notification";
+	createCommentTC,
+	deleteCommentTC,
+	fetchCommentsTC,
+	setIsPaginationCommentsAC,
+	setPageNumberCommentsAC,
+} from 'modules/commentsModule/commentsSlice';
+import { PopUp } from 'common/components/PopUp/PopUp';
+import { Notification } from 'common/components/Notification/Notification';
 
 type PropsType = {
-  postId: string
-}
+	postId: string;
+};
 
-export const Comments:FC<PropsType> = ({postId}) => {
+export const Comments: FC<PropsType> = ({ postId }) => {
+	const comments = useAppSelector(commentsSelector);
 
-  const comments = useAppSelector(commentsSelector)
+	const commentsTotalCount = useAppSelector(commentsTotalCountSelector);
 
-  const commentsTotalCount = useAppSelector(commentsTotalCountSelector)
+	const commentsPageNumber = useAppSelector(commentsPageNumberSelector);
 
-  const commentsPageNumber = useAppSelector(commentsPageNumberSelector)
+	const dispatch = useAppDispatch();
 
-  const dispatch = useAppDispatch()
+	const [content, setContent] = useState('');
 
-  const [content, setContent] = useState('')
+	const [isButtonsShow, setIsButtonsShow] = useState(false);
+	const [isDeletePopUpActive, setIsDeletePopUpActive] = useState(false);
+	const [commentId, setCommentId] = useState('');
+	const onChangeHandler = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
+		setContent(e.currentTarget.value);
+	};
+	const onClickHandler = () => {
+		dispatch(createCommentTC({ postId, content }));
+		setContent('');
+	};
+	const onCommentTypeOnHandler = () => {
+		setIsButtonsShow(true);
+	};
+	const onCommentTypeOffHandler = () => {
+		setIsButtonsShow(false);
+		setContent('');
+	};
 
-  const [isButtonsShow, setIsButtonsShow] = useState(false)
-  const [isDeletePopUpActive, setIsDeletePopUpActive] = useState(false)
-  const [commentId, setCommentId] = useState('')
+	const onPaginationHandler = () => {
+		dispatch(setIsPaginationCommentsAC());
+		dispatch(setPageNumberCommentsAC({ pageNumber: commentsPageNumber + 1 }));
+	};
 
-  const onChangeHandler = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
-    setContent(e.currentTarget.value)
-  }
+	const onDeleteHandler = () => {
+		dispatch(deleteCommentTC({ commentId }));
+	};
 
-  const onClickHandler = () => {
-    dispatch(createCommentTC({postId, content}))
-    setContent('')
-  }
+	useEffect(() => {
+		dispatch(fetchCommentsTC({ postId }));
+	}, [commentsPageNumber, dispatch, postId]);
 
-  const onCommentTypeOnHandler = () => {
-    setIsButtonsShow(true)
-  }
-  const onCommentTypeOffHandler = () => {
-    setIsButtonsShow(false)
-    setContent('')
-  }
-
-  const onPaginationHandler = () => {
-    dispatch(setIsPaginationCommentsAC())
-    dispatch(setPageNumberCommentsAC({pageNumber: commentsPageNumber + 1}))
-  }
-
-  const onDeleteHandler = () => {
-    dispatch(deleteCommentTC({commentId}))
-  }
-
-  useEffect(() => {
-    dispatch(fetchCommentsTC({postId}))
-  }, [commentsPageNumber])
-
-  return (
-    <div className={s.comments_container}>
-      <h3 className={s.header}>{`Comments (${commentsTotalCount || 0})`}</h3>
-      <FormInput value={content} onChange={onChangeHandler} component={'textarea'} onFocus={onCommentTypeOnHandler}/>
-      {isButtonsShow && <div className={s.buttons}>
-        <Button isNoBackGround title="Cancel" callback={onCommentTypeOffHandler}/>
-        <Button disabled={!content} title="Send a Comment" callback={onClickHandler}/>
-      </div>}
-      <div className={s.comments}>
-        {comments.map(cm => <Comment
-          key={cm.id}
-          comment={cm}
-          setPopUpActive={setIsDeletePopUpActive}
-          setCommentId={setCommentId}
-        />)}
-      </div>
-      {commentsTotalCount > comments.length && <div className={s.pagination}><Pagination callback={onPaginationHandler}/></div>}
-      <PopUp isActive={isDeletePopUpActive} setIsActive={setIsDeletePopUpActive}>
-        <Notification
-          title="Delete Comment"
-          message="Are you sure you want to delete comment?"
-          callback={onDeleteHandler}
-          onClose={setIsDeletePopUpActive}
-        />
-      </PopUp>
-    </div>
-  );
+	return (
+		<div className={s.comments_container}>
+			<h3 className={s.header}>{`Comments (${commentsTotalCount || 0})`}</h3>
+			<FormInput
+				value={content}
+				onChange={onChangeHandler}
+				component={'textarea'}
+				onFocus={onCommentTypeOnHandler}
+			/>
+			{isButtonsShow && (
+				<div className={s.buttons}>
+					<Button isNoBackGround title="Cancel" callback={onCommentTypeOffHandler} />
+					<Button disabled={!content} title="Send a Comment" callback={onClickHandler} />
+				</div>
+			)}
+			<div className={s.comments}>
+				{comments.map(cm => (
+					<Comment
+						key={cm.id}
+						comment={cm}
+						setPopUpActive={setIsDeletePopUpActive}
+						setCommentId={setCommentId}
+					/>
+				))}
+			</div>
+			{commentsTotalCount > comments.length && (
+				<div className={s.pagination}>
+					<Pagination callback={onPaginationHandler} />
+				</div>
+			)}
+			<PopUp isActive={isDeletePopUpActive} setIsActive={setIsDeletePopUpActive}>
+				<Notification
+					title="Delete Comment"
+					message="Are you sure you want to delete comment?"
+					callback={onDeleteHandler}
+					onClose={setIsDeletePopUpActive}
+				/>
+			</PopUp>
+		</div>
+	);
 };
 
