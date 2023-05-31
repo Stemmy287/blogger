d5261b85a6ba229f2a7ff2e19d29a1440f3fdc83
stemmy287 <steem287@gmail.com>
2023-05-31 21:50:51 +0300

    feat: active link

--- src/common/components/Link/Link.tsx
+++ src/common/components/Link/Link.tsx
@@ -1,19 +1,23 @@
-import React, {FC, ReactNode} from 'react';
-import s from "./link.module.scss";
-import {NavLink} from "react-router-dom";
+import React, { FC, ReactNode } from 'react';
+import s from './link.module.scss';
+import { NavLink, useLocation } from 'react-router-dom';
 
 type Props = {
-  to: string
-  title: string
-  icon: ReactNode
-}
-
-export const NavBarLink: FC<Props> = ({to, title, icon}) => {
-  return (
-    <NavLink to={to} className={s.navLink}>
-      {icon}
-      <span>{title}</span>
-    </NavLink>
-  );
+	to: string;
+	title: string;
+	icon: ReactNode;
 };
 
+export const Link: FC<Props> = ({ to, title, icon }) => {
+
+	const {pathname} = useLocation()
+
+	const active = pathname === `/${title.toLowerCase()}`
+
+	return (
+		<NavLink to={to} className={active ? `${s.navLink} ${s.active}` : s.navLink}>
+			<div className={s.icon}>{icon}</div>
+			<span>{title}</span>
+		</NavLink>
+	);
+};
--- src/common/components/NavBar/NavBar.tsx
+++ src/common/components/NavBar/NavBar.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import s from 'common/components/NavBar/navBar.module.scss'
-import {PATH} from "common/constans/path";
-import {NavBarLink} from "common/components/Link/Link";
-import {ReactComponent as BurgerMenu} from 'common/icons/BurgerMenuNav.svg'
-import {ReactComponent as SquareMenu} from 'common/icons/SquareMenu.svg'
+import s from 'common/components/NavBar/navBar.module.scss';
+import { PATH } from 'common/constans/path';
+import { Link } from 'common/components/Link/Link';
+import { ReactComponent as BurgerMenu } from 'common/icons/BurgerMenuNav.svg';
+import { ReactComponent as SquareMenu } from 'common/icons/SquareMenu.svg';
 
 export const NavBar = () => {
-    return (
-      <div className={s.navBarContainer}>
-          <NavBarLink to={PATH.BLOGS} title={'Blogs'} icon={<BurgerMenu/>}/>
-          <NavBarLink to={PATH.POSTS} title={'Posts'} icon={<SquareMenu/>}/>
-      </div>
-    );
+	return (
+		<div className={s.navBarContainer}>
+			<Link to={PATH.BLOGS} title={'Blogs'} icon={<BurgerMenu />} />
+			<Link to={PATH.POSTS} title={'Posts'} icon={<SquareMenu />} />
+		</div>
+	);
 };
-
