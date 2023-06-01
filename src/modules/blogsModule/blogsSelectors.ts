f9c500c2edd84e1d04a50310d077139fab4a4927
stemmy287 <steem287@gmail.com>
2023-06-01 16:22:49 +0300

    feat: posts for specific blogs selectors

--- src/modules/blogsModule/blogsSelectors.ts
+++ src/modules/blogsModule/blogsSelectors.ts
@@ -1,15 +1,22 @@
-import {AppRootStateType} from "store/store";
+import { AppRootStateType } from 'store/store';
 
-export const blogsSelector = (state: AppRootStateType) => state.blogs.blogs.items
-export const blogSelector = (state: AppRootStateType) => state.blogs.blog
+export const blogsSelector = (state: AppRootStateType) => state.blogs.blogs.items;
+export const blogSelector = (state: AppRootStateType) => state.blogs.blog;
 
 //meta data
-export const blogsTotalCountSelector = (state: AppRootStateType) => state.blogs.blogs.totalCount
+export const blogsTotalCountSelector = (state: AppRootStateType) => state.blogs.blogs.totalCount;
 
 //query params
-export const blogsPageNumberSelector = (state: AppRootStateType) => state.blogs.queryParams.pageNumber
-export const blogsSortBySelector = (state: AppRootStateType) => state.blogs.queryParams.sortBy
-export const blogsSortDirectionSelector = (state: AppRootStateType) => state.blogs.queryParams.sortDirection
-export const blogsSearchNameTermSelector = (state: AppRootStateType) => state.blogs.queryParams.searchNameTerm
+export const blogsPageNumberSelector = (state: AppRootStateType) => state.blogs.queryParams.pageNumber;
+export const blogsSortBySelector = (state: AppRootStateType) => state.blogs.queryParams.sortBy;
+export const blogsSortDirectionSelector = (state: AppRootStateType) => state.blogs.queryParams.sortDirection;
+export const blogsSearchNameTermSelector = (state: AppRootStateType) => state.blogs.queryParams.searchNameTerm;
 
+//posts for specific blog
+export const postsForSpecificBlogSelector = (state: AppRootStateType) => state.blogs.postsForSpecificBlog.items;
 
+//meta data for posts
+export const postsTotalCountSelector = (state: AppRootStateType) => state.blogs.blogs.totalCount;
+
+//query params for posts
+export const postsPageNumberSelector = (state: AppRootStateType) => state.blogs.queryParamsForPosts.pageNumber;
