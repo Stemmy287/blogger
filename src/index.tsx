0f86fb3e43f19e275185ae5a5117ce70a9160f29
stemmy287 <steem287@gmail.com>
2023-06-05 15:41:50 +0300

    feat: imports

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { index } from 'store';
-import { App } from 'app/App';
+import { App } from 'app';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
