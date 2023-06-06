9adf2c3db7ffe5dfc4f6da7f81aace1e6fa228e0
stemmy287 <steem287@gmail.com>
2023-06-06 17:38:57 +0300

    fix: delete paths

--- src/common/constans/path.ts
+++ src/common/constans/path.ts
@@ -5,7 +5,5 @@ export const PATH = {
   BLOGS: '/blogs',
   POSTS: '/posts',
   BLOG_PAGE: '/blog-page/:blogId?',
-  POST_PAGE: '/post-page/:postId?',
-  ADD_BLOG: '/add-blog',
-  EDIT_BLOG: '/edit-blog'
+  POST_PAGE: '/post-page/:postId?'
 } as const
