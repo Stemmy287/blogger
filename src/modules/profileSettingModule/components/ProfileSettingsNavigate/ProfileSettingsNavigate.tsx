0dcc58324fbe42ddb107d2a0e2fd37a2c1b58913
stemmy287 <steem287@gmail.com>
2023-06-28 20:29:46 +0300

    feat: Profile setting navigate

---
+++ src/modules/profileSettingModule/components/ProfileSettingsNavigate/ProfileSettingsNavigate.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './ProfileSettingsNavigate.module.scss';
+
+export const ProfileSettingsNavigate = () => {
+	return (
+		<div className={s.container}>
+			<h5>Devices</h5>
+		</div>
+	);
+};
