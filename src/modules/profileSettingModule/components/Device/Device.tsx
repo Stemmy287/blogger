6c0187ccb504c26d5ccfb3d270af8cbdf29b35b5
stemmy287 <steem287@gmail.com>
2023-06-29 13:16:14 +0300

    feat: devices layout

--- src/modules/profileSettingModule/components/Device/Device.tsx
+++ src/modules/profileSettingModule/components/Device/Device.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import s from './Device.module.scss';
+import { Logout } from 'common/components';
+import { ReactComponent as Phone } from 'assets/icons/smartphone-svgrepo-com.svg';
+import { ReactComponent as Computer } from 'assets/icons/computer-svgrepo-com.svg';
 
 type PropsType = {
 	title: string;
 	ip: string;
 	date: string;
+	noLogout?: boolean;
 };
 
-export const Device = ({ title, ip, date }: PropsType) => {
+export const Device = ({ title, ip, date, noLogout }: PropsType) => {
 	return (
 		<div className={s.container}>
-			<div className={s.content}>
-				<span className={s.title}>{title}</span>
-				<span className={s.ip}>IP: {ip}</span>
-				<span className={s.date}>Last visit: {date}</span>
+			<div className={s.imgAndContent}>
+				<Computer/>
+				<div className={s.content}>
+					<span className={s.title}>{title}</span>
+					<span className={s.ip}>IP: {ip}</span>
+					<span className={s.date}>Last visit: {date}</span>
+				</div>
 			</div>
+			{!noLogout && <Logout callback={() => {}} />}
 		</div>
 	);
 };
-
--- src/modules/profileSettingModule/components/Devices/Devices.tsx
+++ src/modules/profileSettingModule/components/Devices/Devices.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import { Card } from 'common/components';
+import s from './Devices.module.scss'
+import { Button, Card } from 'common/components';
 import { Device } from 'modules/profileSettingModule';
 
 export const Devices = () => {
 	return (
-		<div>
+		<div className={s.container}>
 			<Card title="This devices">
-				<Device title="Chrome" ip="22.345.345.12" date="22.11.2022" />
+				<Device title="Chrome" ip="22.345.345.12" date="22.11.2022" noLogout />
 			</Card>
+			<Button title="Terminate all other session" isNoBackGround />
+			<div className={s.activeSessions}>
+				<Card title="Active sessions">
+					<Device title="Chrome" ip="22.345.345.12" date="22.11.2022" />
+				</Card>
+				<Card>
+					<Device title="Chrome" ip="22.345.345.12" date="22.11.2022" />
+				</Card>
+				<Card>
+					<Device title="Chrome" ip="22.345.345.12" date="22.11.2022" />
+				</Card>
+			</div>
 		</div>
 	);
 };
